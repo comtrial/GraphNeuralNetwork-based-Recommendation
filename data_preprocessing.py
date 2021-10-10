f8ea2a11114a08126d026a639ea1b4ffc572bb0d
susanee <comtrial97@gmail.com>
2021-10-10 17:21:56 +0900

    first commit

---
+++ data_preprocessing.py
@@ -0,0 +1,13 @@
+import os
+import re
+import argparse
+import pickle
+
+import pandas as pd
+import torch
+
+from builder import PandasGraphBuilder
+
+
+pd.read_csv("/Users/choeseung-won/Deep_Study/Business_Partner_Project/Data/강원_edge.csv")
+
